256e169062e11251c2bcbe3749b545f8c3e8d190
gkell <georgiosk@lerna.ai>
2022-12-16 12:22:01 +0200

    Added Log Uploader
    - Cherry-picking commit 99c8dad
    - Update log file endpoint
    - Optimize Log uploader

--- shared/src/androidMain/kotlin/ai/lerna/multiplatform/FLPeriodicWorker.kt
+++ shared/src/androidMain/kotlin/ai/lerna/multiplatform/FLPeriodicWorker.kt
@@ -1,5 +1,6 @@
 package ai.lerna.multiplatform
 
+import ai.lerna.multiplatform.utils.LogAwsUploaderImpl
 import android.app.Application
 import android.content.Context
 import androidx.concurrent.futures.CallbackToFutureAdapter
@@ -8,13 +9,14 @@ import androidx.work.WorkerParameters
 import com.google.common.util.concurrent.ListenableFuture
 import com.soywiz.korio.android.withAndroidContext
 import io.github.aakira.napier.Napier
+import io.ktor.util.date.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class FLPeriodicWorker(_context: Context, workerParams: WorkerParameters) :
 	ListenableWorker(_context, workerParams) {
-	private val token = inputData.getString("token")
+	private val token = inputData.getString("token") ?: ""
 	private val context = _context
 	private val uniqueID = inputData.getLong("ID", 0L)
 	private val flWorker = FLWorker(token.toString(), uniqueID)
@@ -28,6 +30,7 @@ class FLPeriodicWorker(_context: Context, workerParams: WorkerParameters) :
 					completer.set(Result.success())
 				} catch (e: Exception) {
 					Napier.e("Lerna Worker - Failed $e")
+					LogAwsUploaderImpl(token, FLWorker.FL_WORKER_VERSION).uploadFile(uniqueID, "", e.stackTraceToString(), GMTDate())
 					completer.set(Result.failure())
 				}
 			}
