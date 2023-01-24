835cdfc3767a99d7f87da682cd4cad76824e56e3
gkellaris <georgiosk@lerna.ai>
2023-01-24 11:52:26 -0500

    LERMVP-420 Add iOS sensor data
    - Add Gyroscope and magnetic field.
    - Fixed acceleration.
    - Apply minor optimizations
    - Add rest sersor/data collection
    - Needs coordination with android

--- shared/src/commonMain/kotlin/ai/lerna/multiplatform/config/KMMPreference.kt
+++ shared/src/commonMain/kotlin/ai/lerna/multiplatform/config/KMMPreference.kt
@@ -2,44 +2,44 @@ package ai.lerna.multiplatform.config
 
 import ai.lerna.multiplatform.config.preference.*
 
-class KMMPreference(private val context: KMMContext) {
+internal class KMMPreference(private val context: KMMContext) {
 
-    fun put(key: String, value: Int) {
+    internal fun put(key: String, value: Int) {
         context.putInt(key, value)
     }
 
-    fun put(key: String, value: String) {
+    internal fun put(key: String, value: String) {
         context.putString(key, value)
     }
 
-    fun put(key: String, value: Boolean) {
+    internal fun put(key: String, value: Boolean) {
         context.putBool(key, value)
     }
 
-    fun put(key: String, value: Float) {
+    internal fun put(key: String, value: Float) {
         context.putFloat(key, value)
     }
 
-    fun put(key: String, value: Array<String>) {
+    internal fun put(key: String, value: Array<String>) {
         context.putArray(key, value)
     }
 
-    fun getInt(key: String, default: Int): Int
+    internal fun getInt(key: String, default: Int): Int
             =  context.getInt(key, default)
 
-    fun getFloat(key: String, default: Float): Float
+    internal fun getFloat(key: String, default: Float): Float
             =  context.getFloat(key, default)
 
-    fun getArray(key: String) : Array<String>?
+    internal fun getArray(key: String) : Array<String>?
             =  context.getArray(key)
 
-    fun getString(key: String) : String?
+    internal fun getString(key: String) : String?
             =  context.getString(key)
 
-    fun getBool(key: String, default: Boolean): Boolean =
+    internal fun getBool(key: String, default: Boolean): Boolean =
         context.getBool(key, default)
 
-    fun contains(value: String): Boolean =
+    internal fun contains(value: String): Boolean =
         context.contains(value)
 
 }
--- shared/src/iosMain/kotlin/ai/lerna/multiplatform/Sensors.kt
+++ shared/src/iosMain/kotlin/ai/lerna/multiplatform/Sensors.kt
@@ -1,20 +1,29 @@
 package ai.lerna.multiplatform
 
 import ai.lerna.multiplatform.config.KMMContext
+import io.github.aakira.napier.Napier
 import io.ktor.util.date.*
-import kotlinx.cinterop.CValue
-import kotlinx.cinterop.useContents
+import kotlinx.cinterop.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.launch
+import platform.AVFAudio.*
 import platform.CoreMotion.CMAcceleration
 import platform.CoreMotion.CMAttitudeReferenceFrameXMagneticNorthZVertical
 import platform.CoreMotion.CMDeviceMotion
 import platform.CoreMotion.CMMotionManager
 import platform.Foundation.NSOperationQueue
+import platform.SystemConfiguration.*
+import platform.UIKit.UIDevice
+import platform.UIKit.UIDeviceBatteryState
+import platform.UIKit.UIDeviceOrientationIsLandscape
+import platform.UIKit.UIScreen
+import kotlin.math.PI
+
 
 actual class Sensors actual constructor(_context: KMMContext, _modelData: ModelData) : SensorInterface {
+
     private var _data = Channel<SensorDataInterface?>(Channel.BUFFERED)
     private var modelData = _modelData
 
@@ -27,10 +36,18 @@ actual class Sensors actual constructor(_context: KMMContext, _modelData: ModelD
     private val scope = CoroutineScope(UIDispatcher())
     private val queue = NSOperationQueue.mainQueue
     private val manager = CMMotionManager().apply {
-        deviceMotionUpdateInterval = 20 / 1000.0
+        deviceMotionUpdateInterval = 1.0 //20 / 1000.0
     }
+    private val myDevice = UIDevice.currentDevice
+    private val myScreen = UIScreen.mainScreen
+    private val audioSession = AVAudioSession.sharedInstance()
+
 
     actual override fun start() {
+        myDevice.batteryMonitoringEnabled = true
+        myDevice.proximityMonitoringEnabled = true
+        audioSession.setActive(true, null)
+
         _isEnabled = true
         manager.startDeviceMotionUpdatesUsingReferenceFrame(
             CMAttitudeReferenceFrameXMagneticNorthZVertical, queue
@@ -49,6 +66,7 @@ actual class Sensors actual constructor(_context: KMMContext, _modelData: ModelD
 
     actual override fun stop() {
         _isEnabled = false
+
         manager.stopDeviceMotionUpdates()
     }
 
@@ -57,12 +75,23 @@ actual class Sensors actual constructor(_context: KMMContext, _modelData: ModelD
         val user = motion.userAcceleration
         val gravity = motion.gravity
         val heading = motion.heading
+        val attitude = motion.attitude
+        val magnetic = motion.magneticField
+
+        magnetic.useContents { modelData.setMagneticField(this.field.x.toFloat(), this.field.y.toFloat(), this.field.z.toFloat()) }
+        modelData.setGyroscope(
+            (-attitude.roll * 2.0 / PI).toFloat(),
+            (-attitude.pitch * 2.0 / PI).toFloat(),
+            (-attitude.yaw * 2.0 / PI).toFloat()
+        )
+        user.useContents {
+            modelData.setLinAcceleration(this.x.toFloat(), this.y.toFloat(), this.z.toFloat())
+        }
         return SensorData(heading, convertAcceleration(user), convertAcceleration(gravity))
     }
 
     private fun convertAcceleration(acceleration: CValue<CMAcceleration>): AccelerometerInterface {
         acceleration.useContents {
-            modelData.setLinAcceleration(this.x.toFloat(), this.y.toFloat(), this.z.toFloat())
             return AccelerometerData(this.x, this.y, this.z)
         }
     }
@@ -70,6 +99,58 @@ actual class Sensors actual constructor(_context: KMMContext, _modelData: ModelD
     actual override fun updateData() {
         modelData.setDateTime(GMTDate().hours, GMTDate().dayOfWeek.ordinal)
 
+        val currentRoute = audioSession.currentRoute()
+        val portDescriptionArray = currentRoute.outputs() as List<AVAudioSessionPortDescription>
+
+        val headPhonePortDescriptionArray = portDescriptionArray.filter {  it.portType == AVAudioSessionPortHeadphones  }
+        val isHeadphoneConnected = headPhonePortDescriptionArray.isNotEmpty()
+        modelData.setPhones(isHeadphoneConnected)
+
+        val bluetoothDescriptionArray = portDescriptionArray.filter {  (it.portType == AVAudioSessionPortBluetoothA2DP) || (it.portType == AVAudioSessionPortBluetoothHFP) || (it.portType == AVAudioSessionPortBluetoothLE) }
+        val isBluetoothConnected = bluetoothDescriptionArray.isNotEmpty()
+
+        val speakerDescriptionArray = portDescriptionArray.filter {  it.portType == AVAudioSessionPortBuiltInSpeaker }
+        val isSpeakerOn = speakerDescriptionArray.isNotEmpty()
+
+        val headsetDescriptionArray = portDescriptionArray.filter {  it.portType == AVAudioSessionPortBuiltInReceiver }
+        val isHeadsetOn = headsetDescriptionArray.isNotEmpty()
+
+        val music = audioSession.isOtherAudioPlaying()
+
+        val volume = audioSession.outputVolume()
+
+        modelData.setAudioActivity(2, volume, volume, volume, volume, isBluetoothConnected, false, music, isSpeakerOn, isHeadsetOn)
+
+        val wifi = isWifiConnected()
+        modelData.setWifiConnected(wifi)
+
+        val proximity = if (myDevice.proximityState) {5.0} else {0.0}
+        modelData.setProximity(proximity.toFloat())
+
+        val orientation = if (UIDeviceOrientationIsLandscape(myDevice.orientation)) {1.0} else {0.0}
+        modelData.setOrientation(orientation.toFloat())
+
+        val battery = if (myDevice.batteryState == UIDeviceBatteryState.UIDeviceBatteryStateCharging || myDevice.batteryState == UIDeviceBatteryState.UIDeviceBatteryStateFull) {1} else {0}
+        modelData.setBatteryChargingState(battery)
+
+        modelData.setBrightness(myScreen.brightness.toFloat())
+        modelData.setLight(myScreen.brightness.toFloat())
+
         modelData.setHistory()
+        //Napier.d("Is wifi connected?: ${wifi}, Is music playing?: ${music}, Is headphone connected?: ${isHeadphoneConnected}, Is bluetooth connected?: ${isBluetoothConnected}, Is speaker on?: ${isSpeakerOn}, Is headset on?: ${isHeadsetOn}, Volume: $volume, Proximity: $proximity, orientation: $orientation, battery: $battery, brightness: ${myScreen.brightness}", null, "Lerna Sensors")
+    }
+
+    private fun isWifiConnected(): Boolean {
+        memScoped {
+            val reachability = SCNetworkReachabilityCreateWithName(null, "8.8.8.8")
+            val flags = alloc<SCNetworkReachabilityFlagsVar>()
+            val success = SCNetworkReachabilityGetFlags(reachability, flags.ptr)
+            val isReachable =
+                success && (flags.value.toInt() and kSCNetworkFlagsReachable.toInt() != 0) && flags.value.toInt() and kSCNetworkFlagsConnectionRequired.toInt() == 0
+            val isWifi = success && (flags.value.toInt() and kSCNetworkReachabilityFlagsIsWWAN.toInt() == 0)
+            return isReachable && isWifi
+        }
     }
 }
+
+
