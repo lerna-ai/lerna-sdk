d4aab1cfea4e2ed14d7beb21fa5dfeca1e191bcc
gkell <georgiosk@lerna.ai>
2023-02-20 15:18:17 -0500

    Added multiple inferences
    Also fixed the case where we only predict one class (success/failure) - before it was always choosing success
    
    Needs revisit the percentage of confidence and the return array of multiple inferences

--- shared/src/commonMain/kotlin/ai/lerna/multiplatform/service/MLExecutionImpl.kt
+++ shared/src/commonMain/kotlin/ai/lerna/multiplatform/service/MLExecutionImpl.kt
@@ -121,8 +121,10 @@ class MLExecution(_task: TrainingTasks) : IMLExecution {
 
         for (i in 0 until outputs.values.toList()[0].shape[0]) {
             //Napier.i("predict "+ outputs["1.0"]!!.get(i,0).toString()+", "+ outputs["2.0"]!!.get(i,0).toString()+", "+ outputs["3.0"]!!.get(i,0).toString())
-            var max = 0.0f
+            var max = 0.0f //if more than 1 class, always pick the most probable one even if the probability is very low
             var value = "0.0"
+            if(thetaClass.size==1)
+                max = 0.5f //give success only if confidence is more than 50% (in case we have only 1 class, i.e., success/failure)
             outputs.forEach { (k, _) ->
                 if (outputs[k]!!.asD2Array()[i, 0] > max) {
                     max = outputs[k]!!.asD2Array()[i, 0]
