a34fe238863ea03c822d40f062d3e7c6b4027887
mpoulizos <mpoulizos@gmail.com>
2023-10-05 16:45:45 +0300

    LERMVP-565 Optimize autoconfiguration settings

--- shared/src/commonMain/kotlin/ai/lerna/multiplatform/LernaConfig.kt
+++ shared/src/commonMain/kotlin/ai/lerna/multiplatform/LernaConfig.kt
@@ -4,11 +4,11 @@ import kotlin.Boolean
 import kotlin.String
 
 internal object LernaConfig {
-	internal val CONFIG_SERVER: String = "https://api.dev.lerna.ai:7357/api/v1/"
+	internal val CONFIG_SERVER: String = "https://config.lerna.ai/api/v1/"
 
-	internal val MPC_SERVER: String = "https://api.dev.lerna.ai:3443/"
+	internal val MPC_SERVER: String = "https://api.dev.lerna.ai:8080/"
 
-	internal val FL_SERVER: String = "https://api.dev.lerna.ai:7357/api/v2/"
+	internal val FL_SERVER: String = "https://api.dev.lerna.ai:5000/api/v2/"
 
 	internal val UPLOAD_PREFIX: String = "public/kmm/debug/"
 
