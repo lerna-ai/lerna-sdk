bff866d6a9cf9e9fb3e52bf4849ea773c95bcead
mpoulizos <mpoulizos@gmail.com>
2024-01-02 12:51:14 +0200

    LERMVP-613 Optimize release output
    - Add Release repo
    - Add script for publish android release version
    - Bump kmm to latest version
    - Update usage files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.library").version("8.0.1").apply(false)
-    kotlin("plugin.serialization").version("1.8.21").apply(false)
-    kotlin("multiplatform").version("1.8.21").apply(false)
+    kotlin("plugin.serialization").version("1.9.22").apply(false)
+    kotlin("multiplatform").version("1.9.22").apply(false)
 }
 
 tasks.register("clean", Delete::class) {
